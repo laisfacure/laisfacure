4507a82119eecf6f11b7b6a4c6ffc5c621430f48
laisfacure <159178271+laisfacure@users.noreply.github.com>
2024-05-01 02:13:55 -0300

    Update untitled1.trabalho.lais.direitocentro.py

--- untitled1.trabalho.lais.direitocentro.py
+++ untitled1.trabalho.lais.direitocentro.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Apr 30 14:22:42 2024
 
-@author: 202307164143
+@author: 202307164143, Lais Facure IBMEC Centro Direito
 """
 print("Olá", "mundo", sep="-") # Saída: Olá-mundo
 print("Olá", "Python", end="!\n") # Saída: Olá Python!
@@ -39,4 +39,50 @@ def trinta_por_cento():
         else:
             numeros.append(int(entrada))
     print("Números coletados:", numeros)
-    
+
+
+#7 de maio, exercicio 1.
+def imprimir_informacoes():
+    nome = input("Digite o nome: ")
+    idade = input("Digite a idade: ")
+    cidade = input("Digite a cidade: ")
+    print("Nome:", nome, end=" - ")
+    print("Idade:", idade, end=" - ")
+    print("Cidade:", cidade + "!")
+imprimir_informacoes()
+
+#exercicio 2.
+def calcular_operacao():
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    operacao = input("Digite a operação desejada (+, -, *, /): ")
+
+    if operacao == '+':
+        resultado = num1 + num2
+    elif operacao == '-':
+        resultado = num1 - num2
+    elif operacao == '*':
+        resultado = num1 * num2
+    elif operacao == '/':
+        if num2 != 0:
+            resultado = num1 / num2
+        else:
+            print("Erro: divisão por zero.")
+            return
+
+    print("Resultado:", resultado)
+
+
+calcular_operacao()
+
+#exercicio 3.
+def criar_lista_compras():
+    itens = input("Digite os itens da lista de compras, separados por vírgula: ")
+    lista_compras = itens.split(", ")
+
+    print("Lista de compras:")
+    for i, item in enumerate(lista_compras, start=1):
+        print("Item", i, ":", item)
+
+
+criar_lista_compras()
